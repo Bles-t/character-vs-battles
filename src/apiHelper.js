b1656b94c9e3cb2b31f68da4db3d4be22fa1524f
Adrian Bryant <bryant.adrian94@gmail.com>
2023-12-22 15:58:12 -0500

    Addind API call

---
+++ src/apiHelper.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+
+
+
+function fetchWikiExtract() {
+  const wikiEndpoint = 'https://vsbattles.fandom.com/api.php';
+  const wikiParams = '?action=query'
+    + "&createaccount"
+
+
+
+  const wikiLink = wikiEndpoint + wikiParams;
+
+  console.log(wikiLink);
+
+  var wikiConfig = {
+    timeout: 6500
+  };
+
+
+  async function getJsonResponse(url, config) {
+
+
+    const res = await axios.get(url, config);
+    return res.data;
+
+  }
+  return getJsonResponse(wikiLink, wikiConfig).then(result => {
+    return result
+  }).catch(error => {
+    console.log("an error has occurede" + error);
+    return null;
+  })
+
+}
+
+module.exports = { fetchWikiExtract }
