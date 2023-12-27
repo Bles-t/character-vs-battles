3c7be2a644dcad707df691fbd8a2883a43245ddf
Adrian Bryant <bryant.adrian94@gmail.com>
2023-12-26 22:22:39 -0500

    testign api cLls. point befor eproxy

--- src/apiHelper.js
+++ src/apiHelper.js
@@ -1,6 +1,47 @@
+// import axios from "axios";
+
+
+
+// function fetchWikiExtract() {
+//   const wikiEndpoint = 'https://vsbattles.fandom.com/api.php';
+//   const wikiParams = '?action=query'
+//     + "edit"
+//     + "format=json"
+//     + "feedwatchlist"
+//     + "origin=*"
+//     + "query=parse"
+
+
+
+//   const wikiLink = wikiEndpoint + wikiParams;
+
+//   console.log(wikiLink);
+
+//   var wikiConfig = {
+//     timeout: 6500
+//   };
+
+
+//   async function getJsonResponse(url, config) {
+
+
+//     const res = await axios.get(url, config);
+//     return res.data;
+
+//   }
+//   return getJsonResponse(wikiLink, wikiConfig).then(result => {
+//     return result
+//   }).catch(error => {
+//     console.log("an error has occurede" + error);
+//     return null;
+//   })
+
+// }
+
+// export default  { fetchWikiExtract }
+
+
 import axios from "axios";
-
-
 
 function fetchWikiExtract() {
   const wikiEndpoint = 'https://vsbattles.fandom.com/api.php';
@@ -9,9 +50,7 @@ function fetchWikiExtract() {
     + "format=json"
     + "feedwatchlist"
     + "origin=*"
-    + "query=parse"
-
-
+    + "query=parse";
 
   const wikiLink = wikiEndpoint + wikiParams;
 
@@ -21,21 +60,40 @@ function fetchWikiExtract() {
     timeout: 6500
   };
 
-
   async function getJsonResponse(url, config) {
-
-
     const res = await axios.get(url, config);
     return res.data;
-
   }
-  return getJsonResponse(wikiLink, wikiConfig).then(result => {
-    return result
-  }).catch(error => {
-    console.log("an error has occurede" + error);
-    return null;
-  })
 
+  return getJsonResponse(wikiLink, wikiConfig).then(result => {
+    // Extract power levels from the result
+    const textData = result?.parse?.text["*"];
+    const powerLevels = extractPowerLevels(textData);
+
+    // Do something with the extracted power levels
+    console.log("Power Levels:", powerLevels);
+
+    return result;
+  }).catch(error => {
+    console.log("An error has occurred: " + error);
+    return null;
+  });
 }
 
-export default  { fetchWikiExtract }
+// Function to extract power levels
+function extractPowerLevels(textData) {
+  const regex = /(\w+\s\d+[A-C]-?:.*?)(?=\w+\s\d+[A-C]-?|$)/gs;
+  const matches = textData.match(regex);
+  const powerLevels = {};
+
+  if (matches) {
+    matches.forEach(match => {
+      const [category, description] = match.split(':').map(str => str.trim());
+      powerLevels[category] = description;
+    });
+  }
+
+  return powerLevels;
+}
+
+export default { fetchWikiExtract };
